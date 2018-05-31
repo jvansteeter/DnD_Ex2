287bfbe0210b8311b5c787b8d0705c2df994df64
Clayton Condie <crcondie@verisk.com>
2018-05-31 10:40:42 -0600

    Adjusts css to fill whole page (tested in dev mode)

--- client/src/app/dev-app/dev-app.component.ts
+++ client/src/app/dev-app/dev-app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'dev-app',
-	template: `<board-zone></board-zone>`
+	template: `<board-zone></board-zone>`,
+	styleUrls: ['dev-app.component.css']
 })
 export class DevAppComponent {
 
