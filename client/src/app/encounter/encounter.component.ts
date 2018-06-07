0998cf16c085ed7d5c79a428ab04ea4158e099c8
Joshua Van Steeter <spam.magnet1991@gmail.com>
2018-06-06 21:10:21 -0600

    Fix styles so board map works in shell

--- client/src/app/encounter/encounter.component.ts
+++ client/src/app/encounter/encounter.component.ts
@@ -1,26 +1,27 @@
-import { Component, OnInit } from "@angular/core";
-import { EncounterService } from "./encounter.service";
-import { ActivatedRoute } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { EncounterService } from './encounter.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-    selector: 'encounter',
-    templateUrl: 'encounter.component.html'
+	selector: 'encounter',
+	templateUrl: 'encounter.component.html',
+  styleUrls: ['encounter.component.css']
 })
 export class EncounterComponent implements OnInit {
-    public ready: boolean = false;
+	public ready: boolean = false;
 
-    constructor(private encounterService: EncounterService,
-                private activatedRoute: ActivatedRoute) {
+	constructor(private encounterService: EncounterService,
+							private activatedRoute: ActivatedRoute) {
 
-    }
+	}
 
-    ngOnInit(): void {
-        this.activatedRoute.params.subscribe((params) => {
-            let encounterId = params['encounterId'];
-            this.encounterService.setEncounterId(encounterId);
-            this.encounterService.isReady().subscribe((isReady: boolean) => {
-                this.ready = isReady;
-            });
-        });
-    }
+	ngOnInit(): void {
+		this.activatedRoute.params.subscribe((params) => {
+			let encounterId = params['encounterId'];
+			this.encounterService.setEncounterId(encounterId);
+			this.encounterService.isReady().subscribe((isReady: boolean) => {
+				this.ready = isReady;
+			});
+		});
+	}
 }
