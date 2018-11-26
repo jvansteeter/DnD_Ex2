0355c67bfb64fd1233e39fbf54639ea774bcacae
Clayton Condie <crcondie@verisk.com>
2018-11-26 15:49:16 -0700

    removes an annoying console log

--- client/src/app/board/renderer/light-renderer/light-renderer.component.ts
+++ client/src/app/board/renderer/light-renderer/light-renderer.component.ts
@@ -1,22 +1,19 @@
 import {ViewMode} from '../../shared/enum/view-mode';
 import {BoardStateService} from '../../services/board-state.service';
 import {BoardCanvasService} from '../../services/board-canvas.service';
-import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {BoardLightService} from '../../services/board-light.service';
 import {BoardPlayerService} from "../../services/board-player.service";
-import { EncounterService } from '../../../encounter/encounter.service';
-import {isDefined} from "@angular/compiler/src/util";
+import {EncounterService} from '../../../encounter/encounter.service';
 
 @Component({
     selector: 'light-renderer',
     templateUrl: 'light-renderer.component.html'
 })
 export class LightRendererComponent implements OnInit, OnDestroy {
-    @ViewChild('lightRenderCanvasDark') lightRenderCanvasDark: ElementRef;
-    @ViewChild('lightRenderCanvasDim') lightRenderCanvasDim: ElementRef;
+    @ViewChild('lightRenderCanvasRoot') lightRenderCanvasRoot: ElementRef;
+    private ctx_root: CanvasRenderingContext2D;
 
-    private ctx_dark: CanvasRenderingContext2D;
-    private ctx_dim: CanvasRenderingContext2D;
     private frameId;
 
     constructor(
@@ -25,70 +22,41 @@ export class LightRendererComponent implements OnInit, OnDestroy {
         private boardCanvasService: BoardCanvasService,
         private boardLightService: BoardLightService,
         private encounterService: EncounterService,
-    ) {}
+    ) {
+    }
 
     ngOnInit() {
-        this.ctx_dark = this.lightRenderCanvasDark.nativeElement.getContext('2d');
-        this.ctx_dim = this.lightRenderCanvasDim.nativeElement.getContext('2d');
+        this.ctx_root = this.lightRenderCanvasRoot.nativeElement.getContext('2d');
+
         this.render();
     }
 
     ngOnDestroy(): void {
-    	cancelAnimationFrame(this.frameId);
+        cancelAnimationFrame(this.frameId);
     }
 
     render = () => {
-        this.boardCanvasService.clear_canvas(this.ctx_dark);
-        this.boardCanvasService.clear_canvas(this.ctx_dim);
-
-        this.boardCanvasService.updateTransform(this.ctx_dark);
-        this.boardCanvasService.updateTransform(this.ctx_dim);
+        this.boardCanvasService.clear_canvas(this.ctx_root);
+        this.boardCanvasService.updateTransform(this.ctx_root);
 
         if (this.encounterService.config.lightEnabled) {
-            switch (this.boardStateService.board_view_mode) {
-                case ViewMode.BOARD_MAKER:
-                    this.boardCanvasService.fill_canvas(this.ctx_dim, 'rgba(0, 0, 0, 0.25)');
-                    this.boardCanvasService.fill_canvas(this.ctx_dark, 'rgba(0, 0, 0, 0.45)');
 
-                    for (let lightSource of [...this.boardLightService.lightSources, ...this.boardPlayerService.player_lightSource_map.values()]) {
-                        this.boardCanvasService.clear_xy_array(this.ctx_dark, lightSource.dim_polygon);
-                        this.boardCanvasService.clear_xy_array(this.ctx_dim, lightSource.bright_polygon);
-                    }
-                    break;
-                case ViewMode.PLAYER:
-                    this.boardCanvasService.fill_canvas(this.ctx_dim, 'rgba(0, 0, 0, 0.75)');
-                    this.boardCanvasService.fill_canvas(this.ctx_dark, 'rgba(0, 0, 0, 1.0)');
+            if (this.boardLightService.canvas_rebuild_lightSources) {
+                this.boardCanvasService.fill_canvas(this.boardLightService.canvas_dim_context, 'rgba(0, 0, 0, 1)');
+                this.boardCanvasService.fill_canvas(this.boardLightService.canvas_dark_context, 'rgba(0, 0, 0, 1)');
 
-                    for (let lightSource of this.boardLightService.lightSources) {
-                        this.boardCanvasService.clear_xy_array(this.ctx_dark, lightSource.dim_polygon);
-                        this.boardCanvasService.clear_xy_array(this.ctx_dim, lightSource.bright_polygon);
-                    }
-
-                    for (let player of this.boardPlayerService.players) {
-                        if (player.isVisible) {
-                            let playerVision = player.characterData.values['Vision'];
-                            if (isDefined(playerVision)) {
-                                if (playerVision > 0) {
-                                    let lightSource = this.boardPlayerService.player_lightSource_map.get(player._id);
-                                    if (isDefined(lightSource)) {
-                                        this.boardCanvasService.clear_xy_array(this.ctx_dark, lightSource.dim_polygon);
-                                        this.boardCanvasService.clear_xy_array(this.ctx_dim, lightSource.bright_polygon);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    break;
-                case ViewMode.MASTER:
-                    this.boardCanvasService.fill_canvas(this.ctx_dim, 'rgba(0, 0, 0, 0.25)');
-                    this.boardCanvasService.fill_canvas(this.ctx_dark, 'rgba(0, 0, 0, 0.45)');
-
-                    for (let lightSource of [...this.boardLightService.lightSources, ...this.boardPlayerService.player_lightSource_map.values()]) {
-                        this.boardCanvasService.clear_xy_array(this.ctx_dark, lightSource.dim_polygon);
-                        this.boardCanvasService.clear_xy_array(this.ctx_dim, lightSource.bright_polygon);
-                    }
-                    break;
+                for (let lightSource of [...this.boardLightService.lightSources, ...this.boardPlayerService.player_lightSource_map.values()]) {
+                    this.boardCanvasService.clear_xy_array(this.boardLightService.canvas_dark_context, lightSource.dim_polygon);
+                    this.boardCanvasService.clear_xy_array(this.boardLightService.canvas_dim_context, lightSource.bright_polygon);
+                }
+                this.boardLightService.canvas_rebuild_lightSources = false;
             }
+
+            this.ctx_root.drawImage(this.boardLightService.canvas_dark, 0, 0);
+
+            this.ctx_root.globalAlpha = 0.5;
+            this.ctx_root.drawImage(this.boardLightService.canvas_dim, 0, 0);
+            this.ctx_root.globalAlpha = 1.0;
         }
 
         this.frameId = requestAnimationFrame(this.render);
