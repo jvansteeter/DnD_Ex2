a70501081302883d9b20a875aa7057ce5f9861a6
Joshua Van Steeter <jvansteeter@verisk.com>
2019-08-09 15:55:37 -0600

    Standardize icon list

--- client/src/app/character-sheet/maker/dialog/add-tooltip-aspect.component.ts
+++ client/src/app/character-sheet/maker/dialog/add-tooltip-aspect.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject } from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
 import { Aspect } from '../../shared/aspect';
 import { AlertService } from '../../../alert/alert.service';
+import { IconList } from '../../../utilities/icon-list';
 
 @Component({
 	templateUrl: 'add-tooltip-aspect.component.html',
@@ -12,77 +13,7 @@ export class AddTooltipAspectComponent {
 	public selectedLabel: string;
 	public selectedIcon: string;
 
-	public iconOptions = [
-			'account_balance',
-			'account_circle',
-			'account_box',
-			'all_out',
-			'visibility',
-			'bookmark',
-			'book',
-			'check_circle',
-			'done',
-			'face',
-			'fingerprint',
-			'favorite',
-			'extension',
-			'find_in_page',
-			'gavel',
-			'grade',
-			'home',
-			'hourglass_empty',
-			'hourglass_full',
-			'https',
-			'label',
-			'label_important',
-			'language',
-			'offline_bolt',
-			'opacity',
-			'pets',
-			'settings',
-			'supervisor_account',
-			'turned_in',
-			'visibility',
-			'visibility_off',
-			'error',
-			'call_made',
-			'vpn_key',
-			'add',
-			'clear',
-			'create',
-			'block',
-			'link',
-			'flag',
-			'brightness_high',
-			'brightness_low',
-			'gps_fixed',
-			'gps_not_fixed',
-			'attach_money',
-			'monetization_on',
-			'scatter_plot',
-			'short_text',
-			'cloud',
-			'keyboard_arrow_up',
-			'keyboard_arrow_down',
-			'keyboard_arrow_left',
-			'keyboard_arrow_right',
-			'security',
-			'brightness_2',
-			'brightness_3',
-			'brush',
-			'blur_on',
-			'color_lens',
-			'details',
-			'flash_on',
-			'texture',
-			'local_cafe',
-			'local_drink',
-			'local_dining',
-			'near_me',
-			'ac_unit',
-			'whatshot',
-			'public'
-	];
+	public iconOptions = IconList;
 
 	private iconHasBeenSelected: boolean = false;
 
--- client/src/app/rule-set/home/dialog/new-damage-type-dialog.component.ts
+++ client/src/app/rule-set/home/dialog/new-damage-type-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
 import { DamageTypeData } from '../../../../../../shared/types/rule-set/damage-type.data';
+import { IconList } from '../../../utilities/icon-list';
 
 @Component({
 	templateUrl: 'new-damage-type-dialog.component.html',
@@ -15,77 +16,7 @@ export class NewDamageTypeDialogComponent {
 	public iconOptions: string[];
 
 	constructor(private matDialogRef: MatDialogRef<NewDamageTypeDialogComponent>) {
-		this.iconOptions = [
-			'account_balance',
-			'account_circle',
-			'account_box',
-			'all_out',
-			'visibility',
-			'bookmark',
-			'book',
-			'check_circle',
-			'done',
-			'face',
-			'fingerprint',
-			'favorite',
-			'extension',
-			'find_in_page',
-			'gavel',
-			'grade',
-			'home',
-			'hourglass_empty',
-			'hourglass_full',
-			'https',
-			'label',
-			'label_important',
-			'language',
-			'offline_bolt',
-			'opacity',
-			'pets',
-			'settings',
-			'supervisor_account',
-			'turned_in',
-			'visibility',
-			'visibility_off',
-			'error',
-			'call_made',
-			'vpn_key',
-			'add',
-			'clear',
-			'create',
-			'block',
-			'link',
-			'flag',
-			'brightness_high',
-			'brightness_low',
-			'gps_fixed',
-			'gps_not_fixed',
-			'attach_money',
-			'monetization_on',
-			'scatter_plot',
-			'short_text',
-			'cloud',
-			'keyboard_arrow_up',
-			'keyboard_arrow_down',
-			'keyboard_arrow_left',
-			'keyboard_arrow_right',
-			'security',
-			'brightness_2',
-			'brightness_3',
-			'brush',
-			'blur_on',
-			'color_lens',
-			'details',
-			'flash_on',
-			'texture',
-			'local_cafe',
-			'local_drink',
-			'local_dining',
-			'near_me',
-			'ac_unit',
-			'whatshot',
-			'public'
-		];
+		this.iconOptions = IconList;
 	}
 
 	public selectIcon(icon: string): void {
